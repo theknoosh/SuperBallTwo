2b0323094c30cd97d8cc07f6d4dde68163b40cb2
DPayne <dpayne@Darrells-MacBook-Pro-15.local>
2012-05-09 09:00:33 -0700

    Initial Commit

---
+++ SuperBallTwo/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  SuperBallTwo
+//
+//  Created by Darrell Payne on 5/9/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ SuperBallTwo/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SuperBallTwo
+//
+//  Created by Darrell Payne on 5/9/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
